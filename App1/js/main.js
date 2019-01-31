00b7bba249b20a9103d4840cca2198f2484f3328
visualstudio <visualstudio@230-PC18>
2019-01-31 11:45:18 -0500

    body onload event

--- App1/js/main.js
+++ App1/js/main.js
@@ -1,1 +1,6 @@
 ﻿// Your code here!
+function myFunction() {
+    var date = new Date();
+
+    document.body.innerHTML = "<h1> today is :  " + (date.getMonth() + 1) + " / " + date.getDate() + " / " + date.getFullYear() + "</h>"
+}
